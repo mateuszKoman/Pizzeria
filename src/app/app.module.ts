03a0ce2bc0d9325d7fb9bdf9d7598b4b6551bd05
Mateusz Koman <mateusz.koman@softwareplant.com>
2023-10-10 00:47:59 +0200

    Footer and Input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { WelcomePageComponent } from 'src/app/pages/welcomePage/welcomePage.comp
 import { ShoppingCartPageComponent } from 'src/app/pages/shoppingCartPage/shoppingCartPage.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { CommonModule } from '@angular/common';
+import { InputComponent } from 'src/app/input/input.component';
+import { FooterComponent } from 'src/app/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomePageComponent,
     ShoppingCartPageComponent,
-    HeaderComponent
+    HeaderComponent,
+    InputComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
